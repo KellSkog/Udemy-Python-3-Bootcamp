ca10add14b41d994aab46b87e915238851df861c
KellSkog <kell.skog@gmail.com>
2019-02-07 20:06:50 +0100

    Add formats module

---
+++ 00-PythonObjectAndDataStructureBasics/formats.py
@@ -0,0 +1,5 @@
+def formats(thingy):
+	print(f'I have a {thingy}!')
+	
+if __name__ == "__main__":
+	formats('Bell')
--- 00-PythonObjectAndDataStructureBasics/helloworld.py
+++ 00-PythonObjectAndDataStructureBasics/helloworld.py
@@ -1,2 +1,10 @@
-print('Hello World')
-print("Another Line")
+print('row 1')
+import formats
+def printing():
+	print('Hello World')
+	print("Another Line")
+print('before main')
+if __name__ == "__main__":
+	printing()
+	formats.formats('umbrella')
+print('After main')
