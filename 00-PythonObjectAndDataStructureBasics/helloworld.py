746df29631c5a16ee5a4fde3f0ec8a63c932909f
KellSkog <kell.skog@gmail.com>
2019-02-09 10:51:09 +0100

    Section 3, lecture 20
    https://www.udemy.com/complete-python-bootcamp/learn/v4/t/lecture/9388534?start=0

--- 00-PythonObjectAndDataStructureBasics/helloworld.py
+++ 00-PythonObjectAndDataStructureBasics/helloworld.py
@@ -1,12 +1,4 @@
-#print('row 1')
-from formats import formats
 import listigt
-def printing():
-	print('Hello World')
-	print("Another Line")
-#print('before main')
+
 if __name__ == "__main__":
-#	printing()
-#	formats('umbrella')
 	listigt.concatenate()
-print('After main')
--- 00-PythonObjectAndDataStructureBasics/listigt.py
+++ 00-PythonObjectAndDataStructureBasics/listigt.py
@@ -1,9 +1,17 @@
+# f'..' requires python 3.6
 def concatenate():
 	alfa = ['a', 'b']
 	num = [1, 2]
-	print(alfa + num)
+	print(f'Concatenated {alfa + num}')
 	alfanum = alfa
 	alfanum.append(num.pop())
 	alfanum.append(num.pop())
-	print(alfanum)
-	print(num)
+	print('Concat reversed {val}'.format(val = alfanum))
+	print(f'Empty {num}')
+	
+	myUnsorted = [5,3,7,2,0,9,1]
+	myUnsorted.sort()#myUnsorted is now sorted, sort returns 'None'
+	print(f'Sorted {myUnsorted}')
+	myUnsorted.reverse()
+	print('Reversed {val}'.format(val = myUnsorted))
+	
