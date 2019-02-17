fd9ca0b846a53bf2f2710fb35f7c1727dba3fc04
KellSkog <kell.skog@gmail.com>
2019-02-17 09:23:03 +0100

    Add section 4 (empty)

---
+++ course.py
@@ -0,0 +1,4 @@
+import section4
+
+if __name__ == "__main__":
+	section4.comparison()
---
+++ section4.py
@@ -0,0 +1,8 @@
+def comparison():
+	print('Not a problem')
+	
+def chaining():
+	
+	
+if __name__ == '__main__':
+	print("Hello from Section 4!")
