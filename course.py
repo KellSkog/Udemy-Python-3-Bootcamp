8a6786049847cf34e72534f4d00cd76a8fdfafbd
KellSkog <kell.skog@gmail.com>
2019-02-24 13:44:39 +0100

    03-Function Practice Exercises

--- course.py
+++ course.py
@@ -9,6 +9,61 @@ if __name__ == "__main__":
 	# print(f"Dog? {section6.isDogInString('only cats in this one')}")
 	# print(f"Dog? {section6.isDogInString('this one has a small dog')}")
 	
-	print(section6.pigLatin('word'))
-	print(section6.pigLatin('apple'))
-	print(section6.pigLatin('apple word apple'))
+	# print(section6.pigLatin('word'))
+	# print(section6.pigLatin('apple'))
+	# print(section6.pigLatin('apple word apple'))
+	
+	# section6.varArgs(1,2,3,4,5)
+	# section6.keyWordArguments(tool= 'hammer')
+	
+	# section6.argumentation(0, 3.14, 'Hello', element = 'Boron', liquid = 'water', cost = 5)
+	# print(section6.skyline('Hello world'))
+	
+	assert section6.lesser_of_two_evens(2, 4) == 2
+	assert section6.lesser_of_two_evens(6, 4) == 4
+	assert section6.lesser_of_two_evens(2, 1) == 2
+	assert section6.lesser_of_two_evens(1, 2) == 2
+	assert section6.lesser_of_two_evens(3, 5) == 5
+	
+	assert section6.animal_crackers('Levelheaded Llama')
+	assert not section6.animal_crackers('Crazy Kangaroo')
+	
+	assert section6.makes_twenty(20, 5)
+	assert section6.makes_twenty(5, 20)
+	assert section6.makes_twenty(10, 10)
+	assert not section6.makes_twenty(5, 5)
+	assert section6.makes_twenty(20, 20)
+	
+	assert section6.old_macdonald('macdonald') == 'MacDonald'
+	
+	assert section6.master_yoda('I am home') == 'home am I'
+	assert section6.master_yoda('We are ready') == 'ready are We'
+	
+	assert section6.almost_there(90) == True
+	assert section6.almost_there(104) == True
+	assert section6.almost_there(150) == False
+	assert section6.almost_there(209) == True
+	
+	assert section6.has_33([1, 3, 3]) == True
+	assert section6.has_33([1, 3, 1, 3]) == False
+	assert section6.has_33([3, 1, 3]) == False
+	
+	assert section6.paper_doll('Hello') == 'HHHeeellllllooo'
+	assert section6.paper_doll('Mississippi') == 'MMMiiissssssiiissssssiiippppppiii'
+	
+	assert section6.blackjack(5,6,7) == 18
+	assert section6.blackjack(9,9,9) == 'BUST'
+	assert section6.blackjack(9,9,11) == 19
+	
+	assert section6.summer_69([1, 3, 5]) == 9
+	assert section6.summer_69([4, 5, 6, 7, 8, 9]) == 9
+	assert section6.summer_69([2, 1, 6, 9, 11]) == 14
+	assert section6.summer_69([6, 7, 8, 9]) == 0
+	
+	assert section6.spy_game([1,2,4,0,0,7,5]) == True
+	assert section6.spy_game([1,0,2,4,0,5,7]) == True
+	assert section6.spy_game([1,7,2,0,4,5,0]) == False
+	
+	assert section6.count_primes(100) == 25
+	
+	section6.print_big('a')
--- section6.py
+++ section6.py
@@ -30,5 +30,164 @@ def pigLatin(string):
 		result += ' '
 	return result[:-1]
 	
+def varArgs(*argumentTuple):
+	print(type(argumentTuple), sum(argumentTuple))
+	
+def keyWordArguments(**kwargs):
+	print(type(kwargs))
+	if 'tool' in kwargs:
+		print(f"Use a {kwargs['tool']}")
+	else:
+		print("No tools!")
+		
+def argumentation(*args, **kwargs):
+	print("A ratio {num} and liquid {}".format(kwargs['liquid'], num = args[1]))
+	
+def skyline(input):
+#From: https://stackoverflow.com/questions/3678869/pythonic-way-to-combine-two-lists-in-an-alternating-fashion
+#list3 = [sub[i] for i in range(len(list2)) for sub in [list1, list2]] + [list1[-1]]
+	low = input[::2].lower()
+	upp = input[1::2].upper()
+	both = []
+	for index in range(len(low)):
+		both.append(low[index])
+		if index < len(upp):
+			both.append(upp[index])
+	return ''.join(both)
+	
+'''LESSER OF TWO EVENS: Write a function that returns the lesser of two given numbers 
+if both numbers are even, but returns the greater if one or both numbers are odd'''
+def lesser_of_two_evens(a, b):
+	if(a % 2 == 0) and (b % 2 == 0):
+		return min(a, b)
+	else:
+		return max(a, b)
+		
+'''ANIMAL CRACKERS: Write a function takes a two-word string and 
+returns True if both words begin with same letter'''
+def animal_crackers(input):
+	words = input.split()
+	return words[0][0].lower() == words[1][0].lower()
+	
+'''MAKES TWENTY: Given two integers, return True if the sum of the integers is 20 
+or if one of the integers is 20. If not, return False'''
+def makes_twenty(a, b):
+	return (a == 20) or (b == 20) or (a + b == 20)
+	
+'''OLD MACDONALD: Write a function that capitalizes the first and fourth letters of a name'''
+def old_macdonald(name):
+	if len(name) < 4:
+		return ''
+	capList = list(name)
+	capList[0] = capList[0].upper()
+	capList[3] = capList[3].upper()
+	return ''.join(capList)
+	
+'''MASTER YODA: Given a sentence, return a sentence with the words reversed'''
+def master_yoda(sentence):
+	words = sentence.split()
+	reversed = [w +' ' for w in words[:0:-1]]
+	# print('*',reversed, '*', words[0], '*')
+	reversed.append(words[0])
+	return ''.join(reversed)
+	
+'''ALMOST THERE: Given an integer n, return True if n is within 10 of either 100 or 200'''
+def almost_there(n):
+	return (abs(100 - n) <= 10) or (abs(200 - n) <= 10)
+	
+'''Given a list of ints, return True if the array contains a 3 next to a 3 somewhere.'''
+def has_33(ints):
+	found = False
+	for num in ints:
+		if not num == 3:
+			found = False
+		elif found and (num == 3):
+			return True
+		elif num == 3:
+			found = True
+	return False
+
+'''PAPER DOLL: Given a string, return a string where for every character in the original 
+there are three characters'''
+def paper_doll(string):
+	tripple = [x*3 for x in string]
+	return ''.join(tripple)
+	
+'''BLACKJACK: Given three integers between 1 and 11, if their sum is less than or equal to 21, 
+return their sum. If their sum exceeds 21 and there's an eleven, 
+reduce the total sum by 10. Finally, if the sum (even after adjustment) exceeds 21, return 'BUST' '''
+def blackjack(a, b, c):
+	sum = a+b+c
+	thereIsAnEleven = (a == 11) or (b == 11) or (c == 11)
+	if (sum > 21) and thereIsAnEleven:
+		sum -= 10
+	if (sum < 21):
+		return sum
+	else:
+		return 'BUST'
+		
+'''SUMMER OF '69: Return the sum of the numbers in the array, 
+except ignore sections of numbers starting with a 6 and extending to the next 9 
+(every 6 will be followed by at least one 9). Return 0 for no numbers.'''
+def summer_69(numbers):
+	sum = 0
+	ignore = False
+	for num in numbers:
+		if num == 6:
+			ignore = True
+			continue
+		if num == 9:
+			ignore = False
+			continue
+		if not ignore:
+			sum += num
+	return sum
+	
+'''SPY GAME: Write a function that takes in a list of integers and returns 
+True if it contains 007 in order
+e.g.  [0,1,0,2,7] -> True'''
+def spy_game(integers):
+	filtered = [x  for x in integers if x == 0 or x == 7]
+	last = len(filtered) - 2#maximum length including first 0
+	if last < 1:
+		return False
+	bondFound = False
+	for x in range(last):
+		if (filtered[x] == 0) and (filtered[x + 1] == 0) and (filtered[x + 2] == 7):
+			bondFound = True
+			break
+	return bondFound
+'''Return a list of all primes up to and including 'number' '''
+def makePrimes(num):
+	allNums = [x for x in range(num)]
+	primes = []
+	for x in range(2, num):
+		if allNums[x] == 0:
+			continue
+		else:
+			primes.append(x)
+		for y in range(x, num, x):
+			allNums[y] = 0
+			
+	return primes
+	
+'''COUNT PRIMES: Write a function that returns the number of prime numbers that 
+exist up to and including a given number'''
+def count_primes(number):
+#Make a list of all primes up to and including 'number'
+	primeCount = makePrimes(number)
+#Return length of list
+	return len(primeCount)
+font = {'a': '\
+  a  \n\
+ a a \n\
+aaaaa\n\
+a   a\n\
+a   a\n'}
+'''PRINT BIG: Write a function that takes in a single letter, 
+and returns a 5x5 representation of that letter'''
+def print_big(letter):
+	print(font[letter])
+	
 if __name__ == '__main__':
 	print("Methods and functions")
